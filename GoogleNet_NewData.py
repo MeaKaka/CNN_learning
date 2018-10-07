667b8c82115d94d34efbf82e336b9c18031062ba
MeaKaka <3078858621@qq.com>
2018-10-07 10:00:35 +0800

    Update and rename GoogleNet_NewData to GoogleNet_NewData.py

---
+++ GoogleNet_NewData.py
@@ -0,0 +1,230 @@
+import sys
+sys.path.append('..')
+
+import numpy as np
+import torch
+from torch import nn
+from torch.autograd import Variable
+# from torchvision.datasets import CIFAR10
+import os
+
+# 定义一个卷积加一个 relu 激活函数和一个 batchnorm 作为一个基本的层结构
+def conv_relu(in_channel, out_channel, kernel, stride=1, padding=0):
+    layer = nn.Sequential(
+        nn.Conv2d(in_channel, out_channel, kernel, stride, padding),
+        nn.BatchNorm2d(out_channel, eps=1e-3),
+        nn.ReLU(True)
+    )
+    return layer
+    
+class inception(nn.Module):
+    def __init__(self, in_channel, out1_1, out2_1, out2_3, out3_1, out3_5, out4_1):
+        super(inception, self).__init__()
+        # 第一条线路
+        self.branch1x1 = conv_relu(in_channel, out1_1, 1)
+        
+        # 第二条线路
+        self.branch3x3 = nn.Sequential( 
+            conv_relu(in_channel, out2_1, 1),
+            conv_relu(out2_1, out2_3, 3, padding=1)
+        )
+        
+        # 第三条线路
+        self.branch5x5 = nn.Sequential(
+            conv_relu(in_channel, out3_1, 1),
+            conv_relu(out3_1, out3_5, 5, padding=2)
+        )
+        
+        # 第四条线路
+        self.branch_pool = nn.Sequential(
+            nn.MaxPool2d(3, stride=1, padding=1),
+            conv_relu(in_channel, out4_1, 1)
+        )
+        
+    def forward(self, x):
+        f1 = self.branch1x1(x)
+        f2 = self.branch3x3(x)
+        f3 = self.branch5x5(x)
+        f4 = self.branch_pool(x)
+        output = torch.cat((f1, f2, f3, f4), dim=1)
+        return output
+        
+   class googlenet(nn.Module):
+    def __init__(self, in_channel, num_classes, verbose=False):
+        super(googlenet, self).__init__()
+        self.verbose = verbose
+        
+        self.block1 = nn.Sequential(
+            conv_relu(in_channel, out_channel=64, kernel=7, stride=2, padding=3),
+            nn.MaxPool2d(3, 2)
+        )
+        
+        self.block2 = nn.Sequential(
+            conv_relu(64, 64, kernel=1),
+            conv_relu(64, 192, kernel=3, padding=1),
+            nn.MaxPool2d(3, 2)
+        )
+        
+        self.block3 = nn.Sequential(
+            inception(192, 64, 96, 128, 16, 32, 32),
+            inception(256, 128, 128, 192, 32, 96, 64),
+            nn.MaxPool2d(3, 2)
+        )
+        
+        self.block4 = nn.Sequential(
+            inception(480, 192, 96, 208, 16, 48, 64),
+            inception(512, 160, 112, 224, 24, 64, 64),
+            inception(512, 128, 128, 256, 24, 64, 64),
+            inception(512, 112, 144, 288, 32, 64, 64),
+            inception(528, 256, 160, 320, 32, 128, 128),
+            nn.MaxPool2d(3, 2)
+        )
+        
+        self.block5 = nn.Sequential(
+            inception(832, 256, 160, 320, 32, 128, 128),
+            inception(832, 384, 182, 384, 48, 128, 128),
+            nn.AvgPool2d(2)
+        )
+        
+        self.classifier = nn.Linear(1024, num_classes)
+        
+    def forward(self, x):
+        x = self.block1(x)
+        if self.verbose:
+            print('block 1 output: {}'.format(x.shape))
+        x = self.block2(x)
+        if self.verbose:
+            print('block 2 output: {}'.format(x.shape))
+        x = self.block3(x)
+        if self.verbose:
+            print('block 3 output: {}'.format(x.shape))
+        x = self.block4(x)
+        if self.verbose:
+            print('block 4 output: {}'.format(x.shape))
+        x = self.block5(x)
+        if self.verbose:
+            print('block 5 output: {}'.format(x.shape))
+        x = x.view(x.shape[0], -1)
+        x = self.classifier(x)
+        return x
+        
+   import time
+
+def train_model(model, criterion, optimizer, scheduler, num_epochs=25):
+    since = time.time()
+
+    best_model_wts = model.state_dict()
+    best_acc = 0.0
+
+    for epoch in range(num_epochs):
+        print('Epoch {}/{}'.format(epoch, num_epochs - 1))
+        print('-' * 10)
+
+        # Each epoch has a training and validation phase
+        for phase in ['train', 'val']:
+            if phase == 'train':
+                scheduler.step()
+                model.train(True)  # Set model to training mode
+            else:
+                model.train(False)  # Set model to evaluate mode
+
+            running_loss = 0.0
+            running_corrects = 0
+
+            # Iterate over data.
+            for data in dataloders[phase]:
+                # get the inputs
+                inputs, labels = data
+                inputs, labels = Variable(inputs), Variable(labels)
+
+                # zero the parameter gradients
+                optimizer.zero_grad()
+
+                # forward
+                outputs = model(inputs)
+                _, preds = torch.max(outputs.data, 1)
+                loss = criterion(outputs, labels)
+
+                # backward + optimize only if in training phase
+                if phase == 'train':
+                    loss.backward()
+                    optimizer.step()
+
+                # statistics
+                running_loss += loss.item()
+                running_corrects += torch.sum(preds == labels).item()
+
+            epoch_loss = running_loss / dataset_sizes[phase]
+            epoch_acc = running_corrects / dataset_sizes[phase]
+
+            print('{} Loss: {:.4f} Acc: {:.4f}'.format(
+                phase, epoch_loss, epoch_acc))
+
+            # deep copy the model
+            if phase == 'val' and epoch_acc > best_acc:
+                best_acc = epoch_acc
+                best_model_wts = model.state_dict()
+
+    time_elapsed = time.time() - since
+    print('Training complete in {:.0f}m {:.0f}s'.format(
+        time_elapsed // 60, time_elapsed % 60))
+    print('Best val Acc: {:4f}'.format(best_acc))
+
+    # load best model weights
+    model.load_state_dict(best_model_wts)
+    return model
+from utils import train
+from torchvision import datasets, models, transforms
+from torch.optim import lr_scheduler
+
+
+# def data_tf(x):
+#     x = x.resize((96, 96), 2) # 将图片放大到 96 x 96
+#     x = np.array(x, dtype='float32') / 255
+#     x = (x - 0.5) / 0.5 # 标准化，这个技巧之后会讲到
+#     x = x.transpose((2, 0, 1)) # 将 channel 放到第一维，只是 pytorch 要求的输入方式
+#     x = torch.from_numpy(x)
+#     return x
+   
+data_transforms = {
+    'train': transforms.Compose([
+        transforms.RandomResizedCrop (96),
+        transforms.RandomHorizontalFlip(),
+#         transforms.Resize(96),
+        transforms.ToTensor(),
+        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+    ]),
+    'val': transforms.Compose([
+        transforms.Resize (96),
+        transforms.CenterCrop(96),
+        transforms.ToTensor(),
+        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+    ]),
+}
+
+# your image data file
+data_dir = './CNN_img2/img'
+image_datasets = {x: datasets.ImageFolder(os.path.join(data_dir, x),
+                                          data_transforms[x]) for x in ['train', 'val']}
+# wrap your data and label into Tensor
+dataloders = {x: torch.utils.data.DataLoader(image_datasets[x],
+                                             batch_size=4,
+                                             shuffle=True,
+                                             num_workers=4) for x in ['train', 'val']}
+
+dataset_sizes = {x: len(image_datasets[x]) for x in ['train', 'val']}
+
+
+net = googlenet(3, 3)
+optimizer = torch.optim.SGD(net.parameters(), lr=0.01)
+
+# Decay LR by a factor of 0.1 every 7 epochs
+exp_lr_scheduler = lr_scheduler.StepLR(optimizer, step_size=7, gamma=0.1)
+
+criterion = nn.CrossEntropyLoss()    
+import os
+
+os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+use_gpu = torch.cuda.is_available()
+net = train_model(model=net,criterion=criterion,optimizer=optimizer,scheduler=exp_lr_scheduler,num_epochs=25)
+torch.save(net,'./CNN_img2/googlenet.pt')    
